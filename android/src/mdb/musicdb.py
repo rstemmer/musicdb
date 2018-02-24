06aa8d1a94e48fd2f15969551c9f685f531edec8
Ralf Stemmer <ralf.stemmer@gmx.net>
2018-02-24 16:37:21 +0100

    Implements first artist and album synchronization methods

--- android/src/mdb/musicdb.py
+++ android/src/mdb/musicdb.py
@@ -20,13 +20,83 @@ from mdb.mdbint  import MusicDBInterface
 import os
 
 
+try:
+    import logging
+except:
+    from kivy.logger import Logger as logging
+
+
+
 class MusicDB(MusicDBInterface):
     """
+    Imports self.musicdir, self.artworkdir
     """
 
     def __init__(self, wsurl, httpurl, datadir):
         MusicDBInterface.__init__(self, wsurl, httpurl, datadir)
         self.localdb  = LocalDatabase(os.path.join(datadir, "music.db"))
+
+
+
+    def Synchronize(self):
+        # Get server tables
+        servertables = self.GetDatabaseEntries()
+
+        # Synchronize database
+        self.UpdateArtists(servertables["artists"])
+        self.UpdateAlbums(servertables["albums"])
+
+
+
+    def UpdateArtists(self, serverartists):
+        localartistids = self.localdb.GetAllArtistIDs()
+        for artist in serverartists:
+            artistid = artist["id"]
+
+            # if artist exists, continue with the next one
+            if artistid in localartistids:
+                continue
+
+            # Create new artist entry
+            artist["path"] = str(artistid)   # use the ID as directory
+            self.localdb.CreateArtistEntry(artist)
+
+            # Create artist directory
+            artistdir = os.path.join(self.musicdir, artist["path"])
+            try:
+                os.makedirs(artistdir)
+            except OSError:
+                continue
+
+
+
+    def UpdateAlbums(self, serveralbums):
+        localalbumids = self.localdb.GetAllAlbumIDs()
+        for album in serveralbums:
+            artistid = album["artistid"]
+            albumid  = album["id"]
+
+            # if album exists, continue with the next one
+            if albumid in localalbumids:
+                continue
+
+            # Download artwork
+            serverartworkpath = album["artworkpath"]
+            clientartworkpath = str(album["id"]) + ".jpg"
+            self.DownloadArtworkFile(serverartworkpath, clientartworkpath)
+            album["artworkpath"] = clientartworkpath
+
+            # Create new album entry
+            album["path"] = os.path.join(str(artistid), str(albumid))
+            self.localdb.CreateAlbumEntry(album)
+
+            # Create album directory
+            albumdir = os.path.join(self.musicdir, album["path"])
+            try:
+                os.makedirs(albumdir)
+            except OSError:
+                continue
+
 
 
 
