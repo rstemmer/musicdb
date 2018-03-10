0035378d214ba552989de810bb93ab480bdddeb7
Ralf Stemmer <ralf.stemmer@gmx.net>
2018-03-10 09:49:10 +0100

    Adds documentation

--- android/src/mdb/musicdb.py
+++ android/src/mdb/musicdb.py
@@ -49,6 +49,21 @@ class MusicDB(MusicDBInterface):
 
 
     def UpdateArtists(self, serverartists):
+        """
+        This method compares the local artists with the artists from the server.
+        If the artist does not exists on the local device, a directory named by the artists ID will be created.
+        Then, a new artist entry in the local database will be created.
+
+        If there occurs an error while adding the new artist, it will be skipped.
+        When only creating the artists directory fails, then it only exists in the database.
+        This is OK since one should not trust the file system where every user could remove files.
+
+        Args:
+            serverartists: A list of all rows of the servers artists table
+
+        Returns:
+            *Nothing*
+        """
         localartistids = self.localdb.GetAllArtistIDs()
         for artist in serverartists:
             artistid = artist["id"]
@@ -59,7 +74,9 @@ class MusicDB(MusicDBInterface):
 
             # Create new artist entry
             artist["path"] = str(artistid)   # use the ID as directory
-            self.localdb.CreateArtistEntry(artist)
+            success = self.localdb.CreateArtistEntry(artist)
+            if not success:
+                continue
 
             # Create artist directory
             artistdir = os.path.join(self.musicdir, artist["path"])
@@ -71,6 +88,23 @@ class MusicDB(MusicDBInterface):
 
 
     def UpdateAlbums(self, serveralbums):
+        """
+        This method compared the local albums with the albums from the server.
+        If the album does not exists, it will be created.
+        Creating an album will be done in three steps:
+
+            #. Downloading the artwork from the server into the artwork directory
+            #. Creating an album entry in the local database
+            #. Creating the album directory on the file system
+
+        When one of the steps fails, the other steps gets ignored and the next album will be checked.
+
+        Args:
+            serveralbums: A list of all rows of the servers album table
+
+        Returns:
+            *Nothing*
+        """
         localalbumids = self.localdb.GetAllAlbumIDs()
         for album in serveralbums:
             artistid = album["artistid"]
@@ -83,12 +117,16 @@ class MusicDB(MusicDBInterface):
             # Download artwork
             serverartworkpath = album["artworkpath"]
             clientartworkpath = str(album["id"]) + ".jpg"
-            self.DownloadArtworkFile(serverartworkpath, clientartworkpath)
+            success = self.DownloadArtworkFile(serverartworkpath, clientartworkpath)
+            if not success:
+                continue
             album["artworkpath"] = clientartworkpath
 
             # Create new album entry
             album["path"] = os.path.join(str(artistid), str(albumid))
-            self.localdb.CreateAlbumEntry(album)
+            success = self.localdb.CreateAlbumEntry(album)
+            if not success:
+                continue
 
             # Create album directory
             albumdir = os.path.join(self.musicdir, album["path"])
