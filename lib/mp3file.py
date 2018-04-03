96fadbde025e01860c851a32abf28793112a7498
Ralf Stemmer <ralf.stemmer@gmx.net>
2018-04-03 11:57:38 +0200

    License header updated

--- lib/mp3file.py
+++ lib/mp3file.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+# MusicDB,  a music manager with web-bases UI that focus on music.
+# Copyright (C) 2018  Ralf Stemmer <ralf.stemmer@gmx.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+This module provides a class to read mp3 files frame wise.
+Decoding is not part of it.
+"""
 import sys
 
 BitrateTable = [ # in kilo
