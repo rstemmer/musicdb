b1af0ab3c19eba693ddafc836bdd17b1633cc9b4
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-07-26 06:41:11 +0200

    o+x for frames directory so that the webserver can access it

--- mdbapi/videoframes.py
+++ mdbapi/videoframes.py
@@ -245,7 +245,8 @@ class VideoFrames(object):
             self.framesroot.SetAttributes(dirname,
                     self.cfg.music.owner, self.cfg.music.group,
                     stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
-                    stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP | stat.S_IROTH)
+                    stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP |
+                    stat.S_IROTH |                stat.S_IXOTH )
         except Exception as e:
             logging.warning("Setting frames sub directory attributes failed with error %s. \033[1;30m(Leaving them as they are)", str(e))
 
