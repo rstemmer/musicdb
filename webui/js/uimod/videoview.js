4ad8be01865acb92ac404e764b581d53638e1bef
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-07-26 07:56:54 +0200

    Add proper css to the video view

--- webui/js/uimod/videoview.js
+++ webui/js/uimod/videoview.js
@@ -23,14 +23,22 @@ function ShowVideo(parentID, MDBArtist, MDBAlbum, MDBSong, MDBVideo, MDBTags)
     //window.console && console.log(args);
     let html = "";
 
-    html += "<div id=AlbumViewBox>"; // main box
+    html += "<div id=VideoViewBox>"; // main box
 
     // Album title and artist infos
     html += _VV_CreateHeadline(MDBArtist, MDBAlbum, MDBSong, MDBVideo);
 
-    html += "<div id=ABVSongs>";
-    html += "    <video controls preload=none >";
-    html += "       <source src=\"/musicdb/music/"+MDBVideo.path+"\">";
+    let poster = EncodeVideoThumbnailPath(MDBVideo.framesdirectory, MDBVideo.thumbnailfile);
+    html += "<div id=VVVideo>";
+    html += "    <video ";
+    html += "       class=VV_player";
+    html += "       controls";
+    html += "       width="  + MDBVideo.xresolution;
+    html += "       height=" + MDBVideo.yresolution;
+    html += "       poster=" + poster;
+    html += "       preload=none";
+    html += "       >";
+    html += "       <source src=\"/musicdb/music/" + MDBVideo.path + "\">";
     html += "    </video>";
     html += "</div>";
 
@@ -45,33 +53,33 @@ function ShowVideo(parentID, MDBArtist, MDBAlbum, MDBSong, MDBVideo, MDBTags)
 function _VV_CreateHeadline(MDBArtist, MDBAlbum, MDBSong, MDBVideo)
 {
     let html = "";
-    html += "<div id=ABVHeadlineBox title=\"Origin: " + MDBVideo.origin + "\"";
+    html += "<div id=VVHeadlineBox title=\"Origin: " + MDBVideo.origin + "\"";
     //html += " oncontextmenu=\"ToggleVisibility(\'ABVSettings\'); return false;\""; // Show settings
     html += ">";
 
-    html += "<div id=ABVMainHeadline>";
+    html += "<div id=VVMainHeadline>";
     // Video name
     html += "<span ";
-    html += " id=ABVAlbumName";
+    html += " id=VVAlbumName";
     html += " class=\"fgcolor\">";
     html += MDBVideo.name;
     html += "</span>";
 
     html += "</div>";
-    html += "<div id=ABVSubHeadline class=\"smallfont\">";
+    html += "<div id=VVSubHeadline class=\"smallfont\">";
     // Artist name
     html += "<span ";
     html += " onClick=\"ScrollToArtist(" + MDBArtist.id + ");\"";
-    html += " id=ABVArtistName";
+    html += " id=VVArtistName";
     html += " class=\"hlcolor\">";
     html += MDBArtist.name;
     html += "</span>";
 
-    html += "<span id=ABVSHLSeparator class=\"fgcolor\"> – </span>";
+    html += "<span id=VVSHLSeparator class=\"fgcolor\"> – </span>";
     
     // Video release
     html += "<span ";
-    html += " id=ABVAlbumRelease";
+    html += " id=VVAlbumRelease";
     html += " class=\"hlcolor\">";
     html += MDBVideo.release;
     html += "</span>";
