598b073110e3ca64b59c370fc0d7906994ee2285
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-08-23 16:13:55 +0200

    Use variables for color classes

--- webui/js/style.js
+++ webui/js/style.js
@@ -8,25 +8,14 @@ var lasthlcolor = "#777799";
 // If no arguments, default values (last color setup) will be used
 function UpdateStyle(bgcolor, fgcolor, hlcolor)
 {
-    // Old concept
     lastbgcolor = bgcolor || lastbgcolor;
     lastfgcolor = fgcolor || lastfgcolor;
     lasthlcolor = hlcolor || lasthlcolor;
-    $(".bgcolor").css("background-color", lastbgcolor);
-    $(".fgcolor").css("color",            lastfgcolor);
-    $(".hlcolor").css("color",            lasthlcolor);
-    $(".fmcolor").css("border-color",     lasthlcolor);
 
-    // New concept
     document.documentElement.style.setProperty("--fgcolor", lastfgcolor);
     document.documentElement.style.setProperty("--bgcolor", lastbgcolor);
     document.documentElement.style.setProperty("--hlcolor", lasthlcolor);
     document.documentElement.style.setProperty("--fmcolor", lasthlcolor);
-}
-
-function GetHLColor()
-{
-    return lasthlcolor;
 }
 
 
