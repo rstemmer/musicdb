dc3bc351231c930d347a4704c0bf430f56c4d2d9
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-08-25 12:09:13 +0200

    time to sting and string to time

--- webui/js/tools/Time.js
+++ webui/js/tools/Time.js
@@ -10,28 +10,38 @@ function HMSToString(h,m,s)
     return hs + ":" + ms + ":" + ss;
 }
 
+// mm:ss -> sss (Example: "1:23" -> 83)
+function TimeStringToSeconds(timestring)
+{
+    let substr = timestring.split(":");
+    let min    = substr[0];
+    let sec    = substr[1];
+    let time   = parseInt(min) * 60 + parseInt(sec);
+    return time;
+}
+
+// sss -> mm:ss (Example: 83 -> "1:23")
 function SecondsToTimeString(seconds)
 {
     let time;
-    window.console && console.log(seconds);
-    window.console && console.log(typeof seconds);
     if(typeof seconds === "number")
         time = seconds;
     else if(typeof seconds === "string")
         time = parseFloat(seconds);
 
-    window.console && console.log(time);
     if(isNaN(time))
         return null;
 
-    let min = time / 60;
+    time = Math.round(time);
+
+    let min = Math.floor(time / 60);
     let sec = Math.floor(time % 60);
 
     // convert to string
-    let min_str = ("00"+min).substr(-2);
-    let sec_str = ("00"+sec).substr(-2);
-
-    return min_str + ":" + sec_str;
+    let min_str    = ("  "+min).substr(-2);
+    let sec_str    = ("00"+sec).substr(-2);
+    let timestring = min_str + ":" + sec_str;
+    return timestring;
 }
 
 // vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
