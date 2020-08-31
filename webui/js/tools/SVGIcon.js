16333c8b5e0bd7166f5df7a63ac0b4c6036c756e
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-08-31 10:29:06 +0200

    Toggle button implemented

--- webui/js/tools/SVGIcon.js
+++ webui/js/tools/SVGIcon.js
@@ -34,5 +34,41 @@ class SVGButton extends SVGIcon
     }
 }
 
+
+
+class SVGToggleButton extends SVGIcon
+{
+    constructor(name, onclick)
+    {
+        super(name)
+        this.onclick               = onclick;
+        this.icon.dataset.selected = false;
+        this.icon.onclick          = ()=>{this.onIconClick();};
+    }
+
+    onIconClick()
+    {
+        let oldstate = this.GetSelectionState();
+        let newstate = ! oldstate;
+        this.SetSelectionState(newstate);
+
+        if(typeof this.onclick === "function")
+            this.onclick(newstate);
+
+        return;
+    }
+
+
+    GetSelectionState()
+    {
+        let state = this.icon.dataset.selected;
+        return (state === "true");
+    }
+    SetSelectionState(state)
+    {
+        this.icon.dataset.selected = state;
+    }
+}
+
 // vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
