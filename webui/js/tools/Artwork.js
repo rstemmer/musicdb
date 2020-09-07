d37ce9374f41db95a1ac44fd9aa36f26e56f8a38
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-09-07 17:25:09 +0200

    Separate class for Artworks added

---
+++ webui/js/tools/Artwork.js
@@ -0,0 +1,93 @@
+// MusicDB,  a music manager with web-bases UI that focus on music.
+// Copyright (C) 2017-2020  Ralf Stemmer <ralf.stemmer@gmx.net>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"use strict";
+
+
+class Artwork
+{
+    // size: "small", "medium"
+    constructor(size)
+    {
+        this.element                  = document.createElement("div");
+        this.element.classList.add("Artwork");
+        this.imageelement             = document.createElement("img");
+        this.imageelement.draggable   = false;
+        this.element.appendChild(this.imageelement);
+        this.element.onclick          = ()=>{this.onClick();};
+        this.element.dataset.size     = size;
+    }
+
+
+
+    GetHTMLElement()
+    {
+        return this.element;
+    }
+
+
+
+    // This method should be implemented by a derived class
+    onClick()
+    {
+        window.console && console.warning("This method should have been implemented by a derived class!");
+    }
+}
+
+
+
+class AlbumArtwork extends Artwork
+{
+    constructor(MDBAlbum, size)
+    {
+        super(size);
+    }
+}
+
+
+
+class VideoArtwork extends Artwork
+{
+    constructor(MDBVideo, size)
+    {
+        super(size);
+        
+        this.element.classList.add("VideoArtwork");
+        this.videoid = MDBVideo.id;
+
+        if(size == "small" || size == "medium")
+        {
+            this.anipath = EncodeVideoThumbnailPath(MDBVideo.framesdirectory, MDBVideo.previewfile,   150, 83);
+            this.imgpath = EncodeVideoThumbnailPath(MDBVideo.framesdirectory, MDBVideo.thumbnailfile, 150, 83);
+        }
+        this.imageelement.src         = this.imgpath;
+        this.imageelement.onmouseover = ()=>{this.imageelement.src = this.anipath;};
+        this.imageelement.onmouseout  = ()=>{this.imageelement.src = this.imgpath;};
+    }
+
+
+
+    onClick()
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        MusicDB_Request("GetVideo", "ShowVideo", {videoid: this.videoid});
+    }
+}
+
+
+// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
