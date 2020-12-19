85b6839fedebff414daa2530386cf26081607a43
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-12-19 16:46:03 +0100

    Cleaner code

--- webui/js/tools/CreateGrid.js
+++ webui/js/tools/CreateGrid.js
@@ -8,19 +8,19 @@ class Grid
         this.rows         = rows;
         this.columns      = columns;
         this.tableelement = document.createElement("table");
-        this.tableelement.classList.add("grid_table");
+        this.tableelement.classList.add("grid-table");
         
         this.tablecells   = new Array(); // cells[rows][columns]
         for(let y=0; y<rows; y++)
         {
             let rowelement = document.createElement("tr");
-            rowelement.classList.add("grid_row");
+            rowelement.classList.add("grid-row");
 
             let tablecolumns = new Array();
             for(let x=0; x<columns; x++)
             {
                 let cellelement = document.createElement("td");
-                cellelement.classList.add("grid_cell");
+                cellelement.classList.add("grid-cell");
                 rowelement.appendChild(cellelement);
 
                 tablecolumns.push(cellelement);
@@ -77,36 +77,6 @@ class Grid
 }
 
 
-function CreateGrid(name, columns, rows)
-{
-    var grid = "";
-
-    grid += "<table id=\"GRID_" + name + "\" class=\"grid_table\">";
-    for(let y=0; y<rows; y++)
-    {
-        grid += "<tr class=\"grid_row\">";
-        for(let x=0; x<columns; x++)
-        {
-            let gridid;
-            gridid = GridCellID(name, x, y);
-            grid += "<td id=\"" + gridid + "\" class=\"grid_cell\">";
-            grid += "</td>"
-        }
-        grid += "</tr>"
-    }
-    grid += "</table>"
-
-
-    return grid;
-}
-
-
-function GridCellID(name, x, y)
-{
-    var gridid;
-    gridid = "GRID_" + name + "_" + x + "_" + y;
-    return gridid;
-}
 
 // vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
