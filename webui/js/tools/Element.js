7a902d788ace6fb7128182cb002576a05d281b9d
Ralf Stemmer <ralf.stemmer@gmx.net>
2021-01-14 08:34:44 +0100

    Allow removing childs

--- webui/js/tools/Element.js
+++ webui/js/tools/Element.js
@@ -1,5 +1,5 @@
 // MusicDB,  a music manager with web-bases UI that focus on music.
-// Copyright (C) 2017-2020  Ralf Stemmer <ralf.stemmer@gmx.net>
+// Copyright (C) 2017-2021  Ralf Stemmer <ralf.stemmer@gmx.net>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -46,6 +46,10 @@ class Element
             this.element.appendChild(child.GetHTMLElement());
         else
             this.element.appendChild(child);
+    }
+    RemoveChilds()
+    {
+        this.element.innerHTML = "";
     }
 
 
