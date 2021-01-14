7672042f0c918781ecc08c2946e95f9634ee8ab3
Ralf Stemmer <ralf.stemmer@gmx.net>
2021-01-14 08:18:10 +0100

    EncodePath optimized for general use

--- webui/js/tools/EncodePath.js
+++ webui/js/tools/EncodePath.js
@@ -1,5 +1,5 @@
 // MusicDB,  a music manager with web-bases UI that focus on music.
-// Copyright (C) 2017-2020  Ralf Stemmer <ralf.stemmer@gmx.net>
+// Copyright (C) 2017-2021  Ralf Stemmer <ralf.stemmer@gmx.net>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,11 +16,12 @@
 
 function EncodePath(path)
 {
-    var escpath = "";
-    var start = path.indexOf("/") + 1; // Filename starts behind the "/"
-    escpath  = path.substr(0, start);
-    escpath += encodeURIComponent(path.substr(start));
-    return escpath;
+    let start   = path.lastIndexOf("/") + 1; // File name starts behind the "/"
+    let dirs    = path.substr(0, start);
+    let name    = path.substr(start);
+    let encdirs = encodeURI         (dirs);
+    let encname = encodeURIComponent(name);
+    return encdirs + encname;
 }
 
 
