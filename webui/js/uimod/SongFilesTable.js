b5752f992b0a2ef1c82cf652208cf5b11b1bd8ed
Ralf Stemmer <ralf.stemmer@gmx.net>
2021-12-20 11:11:31 +0100

    Refresh validation when showing the layer

--- webui/js/uimod/SongFilesTable.js
+++ webui/js/uimod/SongFilesTable.js
@@ -352,6 +352,7 @@ class SongFilesTable extends Table
             this.AddRow(row);
         }
         this.AddRow(this.bottomrow);
+        this.onValidationUpdate(); // Refresh validation
     }
 }
 
