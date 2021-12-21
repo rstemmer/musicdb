5191a79f96f719db86f0c1c18c5a0d4edf1a0f96
Ralf Stemmer <ralf.stemmer@gmx.net>
2021-12-21 15:19:00 +0100

    Show task groups and allow collective integration of albumfiles via these groups

--- webui/js/views/TaskListView.js
+++ webui/js/views/TaskListView.js
@@ -23,9 +23,119 @@ class TaskListView extends MainSettingsView
         super("TaskListView", "Open and Running MusicDB Tasks",
             "A list of tasks that are currently processed by the MusicDB server. Tasks can be cancled or further processing steps triggered.");
 
-        this.tasktable = new TaskTable();
+        this.tasktable     = new TaskTable();
+        this.taskgroups    = new Object();
+        this.taskcache     = new Object();
+        this.groupelements = new Object();
 
         this.AppendChild(this.tasktable);
+    }
+
+
+
+    // Group Type: for now only "album"
+    CreateNewGroup(groupid, grouptype)
+    {
+        if(grouptype != "album")
+            return;
+
+        // Headline
+        let headline = new SettingsHeadline(`Temporary Album`,
+            `The following table lists all open tasks for the temporaty album with the ID ${groupid}`);
+
+        // Tool bar for remove and integrate
+        let removebutton    = new SVGButton("Remove", ()=>{this.onRemoveGroup(groupid);},
+            "Remove all tasks and delete all temporary data of this temporary album.");
+        let integratebutton = new SVGButton("MusicDB", ()=>{this.onIntegrateGroup(groupid);},
+            "Try to integrate all files of this temprary album into the Music Directory.");
+
+        let toolbar = new ToolBar();
+        toolbar.AddButton(new ToolGroup([integratebutton]));
+        toolbar.AddSpacer(true /*expand*/);
+        toolbar.AddButton(new ToolGroup([removebutton]));
+
+        // Table
+        let newtable = new TaskTable();
+        this.taskgroups[groupid] = newtable;
+
+        // Append to view
+        this.AppendChild(headline);
+        this.AppendChild(newtable);
+        this.AppendChild(toolbar);
+
+        this.groupelements[groupid] = new Object();
+        this.groupelements[groupid].headline = headline;
+        this.groupelements[groupid].table    = newtable;
+        this.groupelements[groupid].toolbar  = toolbar;
+    }
+
+
+
+    onRemoveGroup(groupid)
+    {
+        let taskids = this.taskgroups[groupid].GetTaskIDs();
+        for(let taskid of taskids)
+            MusicDB_Call("RemoveUpload", {taskid: taskid});
+    }
+
+    onIntegrateGroup(groupid)
+    {
+        let taskids = this.taskgroups[groupid].GetTaskIDs();
+        albumintegrationlayer.ResetUI();
+        for(let taskid of taskids)
+        {
+            let task = this.taskcache[taskid];
+            if(task.state == "readyforintegration")
+                albumintegrationlayer.Integrate(task);
+        }
+        albumintegrationlayer.Show();
+    }
+
+
+
+    UpdateTaskGroups(task)
+    {
+        let groupid = task.annotations.groupid;
+
+        if(typeof groupid !== "string")
+            return;
+
+        if(!(groupid in this.taskgroups))
+        {
+            if(task.contenttype === "albumfile")
+                this.CreateNewGroup(groupid, "album");
+            else
+                return;
+        }
+
+        this.taskgroups[groupid].UpdateTask(task);
+
+        // Remove empty groups
+        let taskids = this.taskgroups[groupid].GetTaskIDs();
+        if(taskids.length === 0)
+        {
+            this.RemoveChild(this.groupelements[groupid].headline);
+            this.RemoveChild(this.groupelements[groupid].table   );
+            this.RemoveChild(this.groupelements[groupid].toolbar );
+            delete this.groupelements[groupid];
+        }
+    }
+
+
+
+    UpdateTask(task)
+    {
+        // Update UI
+        this.tasktable.UpdateTask(task);
+        this.UpdateTaskGroups(task);
+
+        // Cache task
+        let taskid = task.id;
+        let state  = task.state;
+        if(state == "remove" && (taskid in this.taskcache))
+            delete this.taskcache[taskid];
+        else
+            this.taskcache[taskid] = task;
     }
 
 
@@ -35,7 +145,9 @@ class TaskListView extends MainSettingsView
         if(fnc == "GetCurrentTasks" && sig == "ShowCurrentTasks")
         {
             window.console?.log(args);
-            this.tasktable.Update(args);
+            let alltasks = [...args.albumfiles, ...args.videos, ...args.artworks];
+            for(let task of alltasks)
+                this.UpdateTask(task);
         }
         return;
     }
@@ -44,7 +156,7 @@ class TaskListView extends MainSettingsView
     {
         if(fnc == "MusicDB:Task" && sig == "StateUpdate")
         {
-            this.tasktable.UpdateTask(data.task);
+            this.UpdateTask(data.task);
         }
         return;
     }
