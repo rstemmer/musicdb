cee5e8ae7ce52db6b2bdf66ca6f037e32cb2f001
Ralf Stemmer <ralf.stemmer@gmx.net>
2022-01-22 12:40:50 +0100

    Delay scroll to song to make sure the album view has be refreshed correctly

--- webui/js/views/AlbumView.js
+++ webui/js/views/AlbumView.js
@@ -1,5 +1,5 @@
 // MusicDB,  a music manager with web-bases UI that focus on music.
-// Copyright (C) 2017-2021  Ralf Stemmer <ralf.stemmer@gmx.net>
+// Copyright (C) 2017 - 2022  Ralf Stemmer <ralf.stemmer@gmx.net>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -375,7 +375,15 @@ class AlbumView extends MainView2
 
         // Set actual tile to true
         this.songtiles[currentsongid].tile.SetPlayingState(true);
-        this.songtiles[currentsongid].tile.ScrollIntoView();
+
+        // The ScrollIntoView requires the element being rendered.
+        // Especially after a GetAlbum->ShowAlbum message exchange,
+        // the album view may not yet be visible at all.
+        // So this requires a short delay before scrolling to the currently playing song.
+        window.setTimeout(()=>
+            {
+                this.songtiles[currentsongid].tile.ScrollIntoView();
+            }, 500/*ms*/);
         return;
     }
 
