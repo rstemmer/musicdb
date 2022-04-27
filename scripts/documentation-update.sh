ef4338378b4bcb66f012298fbaa299c5e2fc4141
Ralf Stemmer <ralf.stemmer@gmx.net>
2022-04-27 19:07:59 +0200

    Version number updated

--- scripts/documentation-update.sh
+++ scripts/documentation-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p /usr/share/doc/musicdb/htmldoc
-tar -xf ../pkg/musicdb-8.0.0-doc.tar.zst --strip-components=1 -C /usr/share/doc/musicdb/htmldoc
+tar -xf ../pkg/musicdb-8.1.0-doc.tar.zst --strip-components=1 -C /usr/share/doc/musicdb/htmldoc
 
 
