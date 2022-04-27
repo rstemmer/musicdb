6daf48111a02cf694d99ca88c4f23a94c7f6b899
Ralf Stemmer <ralf.stemmer@gmx.net>
2022-04-27 10:16:04 +0200

    Added a function to get the tooltip of an element

--- webui/js/tools/Element.js
+++ webui/js/tools/Element.js
@@ -1,5 +1,5 @@
 // MusicDB,  a music manager with web-bases UI that focus on music.
-// Copyright (C) 2017-2021  Ralf Stemmer <ralf.stemmer@gmx.net>
+// Copyright (C) 2017 - 2022  Ralf Stemmer <ralf.stemmer@gmx.net>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -118,6 +118,10 @@ class Element
     SetTooltip(tooltip)
     {
         this.element.title = tooltip;
+    }
+    GetTooltip()
+    {
+        return this.element.title;
     }
 
 
